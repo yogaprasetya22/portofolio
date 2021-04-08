78bd02e6ff46d5abb7f2ebe942a28c0c118d9196
yogaprasetya22 <yogaagle123z@gmail.com>
2021-04-08 18:23:28 +0700

    oke

--- main.js
+++ main.js
@@ -22,6 +22,10 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
   });
+window.addEventListener("scroll", function () {
+    var header = document.querySelector(".menu");
+    header.classList.toggle("bar", window.scrollY == 0);
+  });
   const navigation = document.querySelector('.menu');
         document.querySelector('.toggle').onclick = function(){
             this.classList.toggle('active');
