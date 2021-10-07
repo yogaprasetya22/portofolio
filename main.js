87d3c92b23f18270dee943e309998fdc690395d3
unknown <yogaprasetya22>
2021-10-07 19:28:54 +0700

    first commit

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 const form = document.querySelector('form');
 
-function send() {
+const send = () => {
   Email.send({
     Host: "smtp.gmail.com",
+    Port: 587,
     Username: "jagres3211@gmail.com",
-    Password: "yogaprasetya3021",
-    To: "yogaagle321z@gmail.com",
+    Password: "viswomowhvgxhhai",
+    To: "jagres3211@gmail.com",
     From: document.getElementById("email").value,
     Subject: document.querySelector("#fnama").value + " mengirim anda pesan",
     Body: document.getElementById("pesan").value + "<br><br><br><br>",
   }).then(
     (response) => form.reset(),
-      Swal.fire({
-        title: document.getElementById("fnama").value,
-        text: "Terima Kasih telah memberi saran",
-        icon: "success",
-      })
+    Swal.fire({
+      title: document.getElementById("fnama").value,
+      text: "Terima Kasih telah memberi saran",
+      icon: "success",
+    })
   );
 }
 window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-  });
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 window.addEventListener("scroll", function () {
-    var header = document.querySelector(".menu");
-    header.classList.toggle("bar", window.scrollY == 0);
-  });
-  const navigation = document.querySelector('.menu');
-        document.querySelector('.toggle').onclick = function(){
-            this.classList.toggle('active');
-            navigation.classList.toggle('active');
-        }
+  var header = document.querySelector(".menu");
+  header.classList.toggle("bar", window.scrollY == 0);
+});
+const navigation = document.querySelector('.menu');
+document.querySelector('.toggle').onclick = function () {
+  this.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
